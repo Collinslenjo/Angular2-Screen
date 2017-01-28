0de346f1a16e46b68054bed81675f962050c4a17
collins lenjo <collinslenjo.cl@gmail.com>
2017-01-28 13:06:37 +0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginFormComponent } from './login/login.component';
+import { RegisterFormComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Welcome';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login/login.component';
+import { RegisterFormComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+  { path: 'login', component: LoginFormComponent},
+  { path: 'register', component: RegisterFormComponent}
+])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
